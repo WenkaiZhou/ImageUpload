2d91fbd20de1d74aa1184da8660005fb027b819c
zhouwk <zwenkai@foxmail.com>
2016-05-19 16:08:48 +0800

    服务端增加接收用户id

--- ImageUploadServer/src/com/kevin/imageuploadserver/UploadImageServlet.java
+++ ImageUploadServer/src/com/kevin/imageuploadserver/UploadImageServlet.java
@@ -2,7 +2,6 @@ package com.kevin.imageuploadserver;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -30,7 +29,10 @@ public class UploadImageServlet extends HttpServlet {
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-			uploadImage(request, response);
+//		接收图片
+		uploadImage(request, response);
+//		接收图片与用户Id
+//		changeUserImage(request, response);
 	}
 
 	// 上传图片文件
@@ -70,6 +72,53 @@ public class UploadImageServlet extends HttpServlet {
 			response.getWriter().write(message);
 		}
 	}
+	
+	// 修改用户的图片
+		private void changeUserImage(HttpServletRequest request, HttpServletResponse response) 
+				throws ServletException, IOException {
+			String message = "";
+			try{
+				DiskFileItemFactory dff = new DiskFileItemFactory();
+				ServletFileUpload sfu = new ServletFileUpload(dff);
+				List<FileItem> items = sfu.parseRequest(request);
+				for(FileItem item:items){
+					if(item.isFormField()){
+						//普通表单
+						String fieldName = item.getFieldName();
+						String fieldValue = item.getString();
+						System.out.println("name="+fieldName + ", value="+ fieldValue);
+					} else {// 获取上传字段
+						// 更改文件名为唯一的
+						String filename = item.getName();
+						if (filename != null) {
+							filename = IdGenertor.generateGUID() + "." + FilenameUtils.getExtension(filename);
+						}
+						// 生成存储路径
+						String storeDirectory = getServletContext().getRealPath("/files/images");
+						File file = new File(storeDirectory);
+						if (!file.exists()) {
+							file.mkdir();
+						}
+						String path = genericPath(filename, storeDirectory);
+						// 处理文件的上传
+						try {
+							item.write(new File(storeDirectory + path, filename));
+							
+							String filePath = "/files/images" + path + "/" + filename;
+							System.out.println("filePath="+filePath);
+							message = filePath;
+						} catch (Exception e) {
+							message = "上传图片失败";
+						}
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				message = "上传图片失败";
+			} finally {
+				response.getWriter().write(message);
+			}
+		}
 	
 	//计算文件的存放目录
 	private String genericPath(String filename, String storeDirectory) {
